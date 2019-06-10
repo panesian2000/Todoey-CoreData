fa2e200bb6505129a95520a8c52cb443eb7d82f8
Ken Loh <ken@proappconcept.com>
2019-06-10 18:20:40 +0800

    Initial Commit

---
+++ Todoey-CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey-CoreData
+//
+//  Created by Kenny Loh on 10/6/19.
+//  Copyright © 2019 ProApp Concept Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
