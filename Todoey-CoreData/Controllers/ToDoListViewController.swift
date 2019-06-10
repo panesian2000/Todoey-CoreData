1a71a1683a76206b97b21be6f1112c1eea8ada56
Ken Loh <ken@proappconcept.com>
2019-06-10 18:39:51 +0800

    Complete UI and Setting.

--- Todoey-CoreData/Controllers/ToDoListViewController.swift
+++ Todoey-CoreData/Controllers/ToDoListViewController.swift
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ToDoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
